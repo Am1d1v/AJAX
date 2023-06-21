0a3bc8ed50c2fa694e14c657d6a3c995c2db20a6
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-06-21 19:10:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+/*
+
 function req(id){
 
     const request = new XMLHttpRequest();
@@ -19,3 +21,22 @@ req(3);
 req('');
 
 console.log('end');
+
+*/
+
+//==================
+
+
+    const req = new XMLHttpRequest();
+    req.open('GET', `https://dummyjson.com/products`);
+    req.send();
+
+    req.addEventListener("load", function(){
+        const {products} = (JSON.parse(this.responseText));
+        console.log(products);
+        const sum = products.reduce((acc, p) => acc += p.price, 0)
+        console.log(sum);
+
+        const average = sum / products.length;
+        console.log(`Average price of products is ${average}`);
+    })
